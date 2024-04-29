c0c33b7b6a2322a84b5d505ca1d5b8416096a54f
Tobey Foxcroft <tobey.foxcroft@students.plymouth.ac.uk>
2024-04-29 11:04:07 +0100

    gameplay refining

--- Source/BoatControls.h
+++ Source/BoatControls.h
@@ -11,6 +11,9 @@
 
 #include <algorithm>
 #include <iostream>
+#include <chrono>
+#include <cstdint>
+#include <ctime>
 
 float Clamp(float value, float min, float max) {
 	if (value < min) {
@@ -24,6 +27,11 @@ float Clamp(float value, float min, float max) {
 	}
 }
 
+int CurrentTime() {
+	unsigned int Tick = std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+	return Tick;
+}
+
 struct CameraData {
 	glm::vec3 CameraPosition;
 	glm::mat4 ViewMatrix;
@@ -32,9 +40,19 @@ struct CameraData {
 class Boat {
 private:
 	float BoatHeight = 0.5f;
-	float BoatSpeed = 1.0f;
+	float BoatSpeed = 2.0f;
 	float BoatRotateSensitivity = 1.0f;
 	float BoatScale = 0.02f;
+	float Smoothing = 0.004f;
+	float RotationSmoothing = 0.003f;
+
+	glm::vec3 LastCameraPosition = glm::vec3(0.0f);
+	glm::vec3 LastCameraOrientation = glm::vec3(0.0f);
+
+	bool IsAccelerating = false;
+	int LastBoatAccelStart = 0;
+	int AccelSpeed = 1;
+	float LastBoatSpeed = 0.0f;
 
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
@@ -52,13 +70,27 @@ public:
 			float CalcSpeed = 1.0f * Delta;
 			float InverseScale = 1.0f / BoatScale;
 			if (KeyPress(GLFW_KEY_W)) {
-				BoatMatrix = glm::translate(BoatMatrix, glm::vec3(0.0f, BoatSpeed * CalcSpeed * InverseScale, 0.0f));
+				if (!IsAccelerating) {
+					IsAccelerating = true;
+					LastBoatAccelStart = CurrentTime();
+				} else {
+					int TimeSinceStart = CurrentTime() - LastBoatAccelStart; // eg 2500
+					if (TimeSinceStart != 0) {
+						float AccelMultiplier = float(TimeSinceStart) / (AccelSpeed * 1000.0f);
+						AccelMultiplier = Clamp(AccelMultiplier, 0.0f, 1.0f);
+						LastBoatSpeed = AccelMultiplier;
+						BoatMatrix = glm::translate(BoatMatrix, glm::vec3(0.0f, BoatSpeed * CalcSpeed * InverseScale * AccelMultiplier, 0.0f));
+					}
+				}
+			}
+			else if (!KeyPress(GLFW_KEY_W)) {
+				IsAccelerating = false;
 			}
 			if (KeyPress(GLFW_KEY_A)) {
-				BoatMatrix = glm::rotate(BoatMatrix, glm::radians(BoatRotateSensitivity * CalcSpeed * InverseScale), glm::vec3(0.0f, 0.0f, 1.0f));
+				BoatMatrix = glm::rotate(BoatMatrix, glm::radians(BoatRotateSensitivity * CalcSpeed * InverseScale * LastBoatSpeed), glm::vec3(0.0f, 0.0f, 1.0f));
 			}
 			if (KeyPress(GLFW_KEY_D)) {
-				BoatMatrix = glm::rotate(BoatMatrix, glm::radians(BoatRotateSensitivity * CalcSpeed * InverseScale), glm::vec3(0.0f, 0.0f, -1.0f));
+				BoatMatrix = glm::rotate(BoatMatrix, glm::radians(BoatRotateSensitivity * CalcSpeed * InverseScale * LastBoatSpeed), glm::vec3(0.0f, 0.0f, -1.0f));
 			}
 		}
 	}
@@ -90,11 +122,19 @@ public:
 		glm::vec3 BoatPosition = glm::vec3(NewMatrix[3]); // extract position from boat matrix
 		glm::mat3 RotationMatrix = glm::mat3(NewMatrix); // extract rotation from boat matrix
 
-		glm::vec3 CameraPosition = BoatPosition - (RotationMatrix * (glm::vec3(0.0f, -0.4f, -1.2f))); // get the camera position of the boat ensuring to move relative to the boat
+		glm::vec3 CameraPosition = BoatPosition - (RotationMatrix * (glm::vec3(0.0f, -0.4f, -1.1f))); // get the camera position of the boat ensuring to move relative to the boat
 		glm::vec3 CameraUp = RotationMatrix * glm::vec3(0.0f, 1.0f, 0.0f); // copy the boats up vector
-		glm::vec3 CameraTarget = glm::vec3(BoatPosition.x, BoatPosition.y + 0.5f, BoatPosition.z); // where the camera aims at
+		glm::vec3 CameraTarget = glm::vec3(BoatPosition.x, BoatPosition.y + 0.4f, BoatPosition.z); // where the camera aims at
 
+		if (LastCameraPosition != glm::vec3(0.0f) && LastCameraOrientation != glm::vec3(0.0f)) {
+			CameraPosition = glm::mix(LastCameraPosition, CameraPosition, Smoothing);
+			CameraTarget = glm::mix(LastCameraOrientation, CameraTarget, RotationSmoothing);
+		}
+
+		LastCameraPosition = CameraPosition;
+		LastCameraOrientation = CameraTarget;
 		glm::mat4 View = glm::lookAt(CameraPosition, CameraTarget, CameraUp); // final calculation
+
 		return {CameraPosition, View};
 	}
 
